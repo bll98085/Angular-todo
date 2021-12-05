fd24180edba3fcca5265dc37e1dc4edaaee95fcd
bll98085 <bll98085@gmail.com>
2021-12-05 17:30:56 +0800

    add item interface

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Item } from '../item';
 
 @Component({
   selector: 'app-todo',
